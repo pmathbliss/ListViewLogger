36d565c344ea7034127238c42cc2655e6835bee2
Paul Bliss <pmathbliss@gmail.com>
2018-05-06 18:03:46 -0700

    Test logging with a background worker. Adds a new list to the list box and keeps the scroll at the bottom.

--- ListViewLogger/Form1.Designer.cs
+++ ListViewLogger/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ListViewLogger
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.gbLogBox = new System.Windows.Forms.GroupBox();
+            this.lbLogData = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.ToolStripButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tspbProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.tsslNumberOfWorkers = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.gbLogBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tspbProgress,
+            this.tsslNumberOfWorkers});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnStart});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.gbLogBox);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 403);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // gbLogBox
+            // 
+            this.gbLogBox.Controls.Add(this.lbLogData);
+            this.gbLogBox.Location = new System.Drawing.Point(3, 3);
+            this.gbLogBox.Name = "gbLogBox";
+            this.gbLogBox.Size = new System.Drawing.Size(797, 397);
+            this.gbLogBox.TabIndex = 0;
+            this.gbLogBox.TabStop = false;
+            this.gbLogBox.Text = "Log";
+            // 
+            // lbLogData
+            // 
+            this.lbLogData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbLogData.FormattingEnabled = true;
+            this.lbLogData.Location = new System.Drawing.Point(3, 16);
+            this.lbLogData.Name = "lbLogData";
+            this.lbLogData.Size = new System.Drawing.Size(791, 378);
+            this.lbLogData.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(51, 22);
+            this.btnStart.Text = "Start";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tspbProgress
+            // 
+            this.tspbProgress.Name = "tspbProgress";
+            this.tspbProgress.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tsslNumberOfWorkers
+            // 
+            this.tsslNumberOfWorkers.Name = "tsslNumberOfWorkers";
+            this.tsslNumberOfWorkers.Size = new System.Drawing.Size(0, 17);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "mainForm";
+            this.Text = "Logger Test";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.gbLogBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox gbLogBox;
+        private System.Windows.Forms.ListBox lbLogData;
+        private System.Windows.Forms.ToolStripButton btnStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripProgressBar tspbProgress;
+        private System.Windows.Forms.ToolStripStatusLabel tsslNumberOfWorkers;
     }
 }
 
--- ListViewLogger/Form1.cs
+++ ListViewLogger/Form1.cs
@@ -2,19 +2,96 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ListViewLogger
 {
-    public partial class Form1 : Form
+    public partial class mainForm : Form
     {
-        public Form1()
+        public mainForm()
         {
             InitializeComponent();
+        }
+
+        private int numberOfWorkers = 0;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            numberOfWorkers++;
+            DisplayNumberOfWorkers();
+            //btnStart.Enabled = false;
+            tspbProgress.Value = 0;
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.ProgressChanged += BackgroundWorker1_ProgressChanged;
+            bw.DoWork += BackgroundWorker1_DoWork;
+            bw.RunWorkerCompleted += BackgroundWorker1_RunWorkerCompleted;
+            bw.WorkerReportsProgress = true;
+            bw.RunWorkerAsync();
+
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            numberOfWorkers--;
+            tspbProgress.Value = 0;
+            // btnStart.Enabled = true;
+            DisplayNumberOfWorkers();
+        }
+
+        private void DisplayNumberOfWorkers()
+        {
+            tsslNumberOfWorkers.Text = numberOfWorkers == 0 ? "" : $"Workers {numberOfWorkers}";
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<string> text = new List<string>();
+            text.Add("Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, ");
+            text.Add("there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the");
+            text.Add(" Semantics, a large language ocean. A small river named Duden flows by their place and supplies");
+            text.Add(" it with the necessary regelialia. It is a paradisematic country, in which roasted parts of ");
+            text.Add("sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind ");
+            text.Add("texts it is an almost unorthographic life One day however a small line of blind text by the name");
+            text.Add(" of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to ");
+            text.Add("do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but ");
+            text.Add("the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt");
+            text.Add(" and made herself on the way. When she reached the first hills of the Italic Mountains, she had ");
+            text.Add("a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village");
+            text.Add(" and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then");
+
+            var worker = sender as BackgroundWorker;
+            var rnd = new Random(DateTime.Now.Millisecond);
+            string startTime = DateTime.Now.Millisecond.ToString();
+            int i = 0;
+            int count = text.Count();
+            foreach(string txt in text)
+            {
+                i++;
+                int ticks = rnd.Next(500, 2000);
+                Debug.WriteLine(ticks);
+                Thread.Sleep(ticks);
+                double progress = Math.Round((double)i / (double)count,2) * (double)100;
+                worker.ReportProgress(Int32.Parse(progress.ToString()), $"[{startTime}] {i.ToString()} {txt}");
+                
+            }
+
+            worker.ReportProgress(100, $"[{startTime}] Finished");
+            
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            DisplayNumberOfWorkers();
+            tspbProgress.Value = e.ProgressPercentage;
+            string message = (string)e.UserState;
+            lbLogData.Items.Add($"[{DateTime.Now.ToString("o")}][{e.ProgressPercentage}] {message}");
+            lbLogData.TopIndex = lbLogData.Items.Count - 1;
         }
     }
 }
